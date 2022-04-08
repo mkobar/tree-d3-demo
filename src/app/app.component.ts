fb0290d58caa940c4389b6469866e6f42a5650d1
mkobar <mike@exlorev.com>
2022-04-08 15:56:26 -0400

    added active map support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './shared/api.service';
+import { ActiveMapService } from './shared/activemap.service';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +8,76 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
-  constructor(){
+export class AppComponent implements OnInit {
+  mapSetting: any
+  emptyData: any = { name: 'nemo' }
+  mapData: any [] = [
+	{ name: 'cars', treeData: 'data', nodes: [], links: [],
+          lastNodeId: 0, root: null },
+  	{ name: 'cats', treeData: 'data', nodes: [], links: [],
+          lastNodeId: 0, root: null },
+  	{ name: 'bats', treeData: 'data', nodes: [], links: [],
+          lastNodeId: 0, root: null }]
+  //treeData: any [] = [ this.emptyData, this.emptyData, this.emptyData ]
+  buttons: string [] = ['cars', 'cats', 'bats']
+ 
+  constructor(public api: ApiService,
+	      public amap: ActiveMapService) { }
 
+  ngOnInit() {
+    this.api.getJSON('car-data.json').subscribe(data => {
+      //this.treeData[0] = data;
+      //this.treeData.push(data);
+      this.mapData[0].treeData = data;
+      console.log('mapData[0]=', this.mapData[0])
+      //this.mapSetting = this.mapData[0] // default
+      this.amap.setMap(this.mapData[0])
+    });
+
+    this.api.getJSON('cat-data.json').subscribe(data => {
+      //this.treeData.push(data);
+      //this.treeData[1] = data;
+      this.mapData[1].treeData = data;
+      console.log('mapData[1]=', this.mapData[1])
+    });
+
+    this.api.getJSON('bat-data.json').subscribe(data => {
+      //this.treeData[2] = data;
+      this.mapData[2].treeData = data;
+      console.log('mapData[2]=', this.mapData[2])
+    });
+
+      //this.initTree();
   }
+
+
+  selectMap (query) {
+    //this.queryNotes = query.notes
+
+    // this.modalReference = this.modalService.open(content, { ariaLabelledBy: 'modal-title' }).result.then((result) => {
+      console.log('selectMap: query=', query)
+      if (query === 'cars') {
+        //this.mapSetting = this.mapData[0]
+        this.amap.setMap(this.mapData[0])
+      } else if (query === 'cats') {
+        //this.mapSetting = this.mapData[1]
+        this.amap.setMap(this.mapData[1])
+      } else if (query === 'bats') {
+        //this.mapSetting = this.mapData[2]
+        this.amap.setMap(this.mapData[2])
+      } else {
+        //this.mapSetting = this.mapData[0]
+        this.amap.setMap(this.mapData[0])
+      }
+
+      // this.trashQuery()
+      // save notes back to query
+      //query.notes = this.queryNotes
+      //this.queryService.updateQuery(query)
+      // this.queryService.updateQuery(this.activeQuery)
+
+    // also select query
+    //this.selectQuery(query)
+  }
+
 }
---
+++ src/app/shared/activeMap.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+//import { environment } from '../../environments/environment';
+//import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs' // eslint-disable-line no-unused-vars
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ActiveMapService {
+
+    private activeMap: any
+
+    private mapChange = new BehaviorSubject<any>(
+      { name: 'empty', treeData: 'data', nodes: 'nodes', links: 'links',
+          lastNodeId: 0 })
+
+
+    private emptyMap: any = 
+      { name: 'empty', treeData: 'data', nodes: 'nodes', links: 'links',
+          lastNodeId: 0 }
+
+    //constructor() { }
+
+    // Observable mapItem stream
+    mapItem$ = this.mapChange.asObservable()
+
+    // service command
+    changeActiveMap (any) {
+      this.mapChange.next(any)
+    }
+
+    //private _jsonURL = 'assets/car-data.json';
+    public getMap(): Observable<any> {
+        //this.activeMap = map
+        //this.activeMap = this.emptyMap
+        return this.activeMap;
+    }
+
+    /**
+    public getActiveMap(name: String): Observable<any> {
+        //this.activeMap = map
+        //return this.activeMap;
+    console.log('getMap')
+    console.log('getMap:name=', name)
+    // detect default selectedQuery here
+    for (const m of queries?) { // of returns objects, in returns ints
+      console.log(m.name, name)
+      if (m.status === name) {
+        this.mapChange.next(m) // force new activeMap?
+        return
+      }
+    }
+    // return empty here if no match?
+    this.queryChange.next(this.emptyQuery) // force empty activeQuery
+    }
+    **/
+
+   /***
+    public setMap(map: any): Observable<any> {
+        this.activeMap = map
+	this.mapChange.next(map) // force new activeMap?
+
+        return this.activeMap;
+    }
+   ***/
+
+    public setMap(map: any) {
+        this.activeMap = map
+	this.mapChange.next(map) // force new activeMap?
+
+        return;
+    }
+}
